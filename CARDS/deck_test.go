be8a7feb104d458e2e01897f733c15811fa66354
Ryan McDougall <rp.mcdougall@gmail.com>
2017-11-08 20:48:37 -0600

    Created tests for deck length, first and last card values. Split from one test function to multiple to see multiple test runs

--- CARDS/deck_test.go
+++ CARDS/deck_test.go
@@ -1,1 +1,30 @@
 package main
+
+import "testing"
+
+//captialize function name to export
+func TestNewDeckLength(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16 but got %v", len(d))
+	}
+}
+
+func TestFirstCard(t *testing.T) {
+	d := newDeck()
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Aced of spades, but got %v", d[0])
+	}
+
+}
+
+func TestLastCard(t *testing.T) {
+	d := newDeck()
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expeceted last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
+
+}
